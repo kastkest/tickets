4ddb9d752b5d9bce7fd69d24e0d824f4a10b067b
kastkest <endimion-xi@yandex.ru>
2022-03-17 11:14:02 +0300

    All method in according Classes

--- src/main/java/com/github/kastkest/idea/Tickets.java
+++ src/main/java/com/github/kastkest/idea/Tickets.java
@@ -1,7 +1,6 @@
 package com.github.kastkest.idea;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
